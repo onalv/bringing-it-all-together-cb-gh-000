b5ef950c396426a3d7fdd197a79c38b57349028a
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 14:40:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,7 +93,7 @@ class Dog
 
     if !search.empty?
       dog_data = search[0]
-      self.new_from_db(dog_data)
+      Dog.new_from_db(dog_data)
     else
       dog = self.create(name: name, breed: breed)
     end
