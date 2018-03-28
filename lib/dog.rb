7dc9a02f4c67a2b6ae923b1f80ab65db8a7077b5
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 13:50:05 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  def initialize(id:, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def attributes
+
+  end
+
+  def self.create_table
+
+  end
+end
