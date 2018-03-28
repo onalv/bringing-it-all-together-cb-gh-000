e63e7e6de4306695c3ada79e9bdb810c73196d44
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 14:21:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
     id = row[0]
     name = row[1]
     breed = row[2]
-    new_dog = self.new(id: id, name: name, breed: breed)
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name(name)
@@ -82,4 +82,6 @@ class Dog
     SQL
     DB[:conn].execute(sql, id).map { |row| self.new_from_db(row)  }.first
   end
+
+  def
 end
